ea67a5dea6631e9dc0e3d2c44b9050d0ef009628
gooseCode <aleksandrbasangov.1994@gmail.com>
2024-03-16 13:06:57 +0200

    feature: itemClass

---
+++ Assets/Scripts/BaseScripts/InventoryItem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem
+{
+    protected string name;
+    protected string imageName;
+    protected Texture2D itemImage;
+    // Start is called before the first frame update
+    InventoryItem(string nameToSet, string imageNameToLoad)
+    {
+        this.name = nameToSet;
+        this.imageName = imageNameToLoad;
+        TextAsset imageBytes= (TextAsset)Resources.Load("ItemSprites/" + imageNameToLoad);
+        this.itemImage= new Texture2D(128, 128);
+        itemImage.LoadImage(imageBytes.bytes);
+
+    }
+    public string GetName()
+    {
+        return this.name;
+    }
+
+    public Texture2D GetImage()
+    {
+        return this.itemImage;
+    }
+}
